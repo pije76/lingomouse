52c9907d952a706bc3497751bc0a4a9801b8ad78
Panjianom <panjul76@hotmail.com>
2022-12-25 14:39:04 +0700

    Edit config template

--- config/urls.py
+++ config/urls.py
@@ -11,4 +11,10 @@ urlpatterns = [
 
     path('country/', country_list, name="country_list"),
     path('language/', language_list, name="language_list"),
+
+    path('country/<int:pk>/change/', country_detail, name='country_detail'),
+    path('language/<int:pk>/change/', language_detail, name='language_detail'),
+
+    path('country/add/', country_add, name='country_add'),
+    path('language/add/', language_add, name='language_add'),
 ]
--- config/views.py
+++ config/views.py
@@ -55,3 +55,140 @@ def language_list(request):
 
     return render(request,'config/config_set.html', context)
 
+
+def country_detail(request, pk):
+    page_title = _('Change Country')
+    course = get_object_or_404(Course, id=pk)
+    word = Word.objects.all()
+
+    form = CourseForm(prefix='course')
+
+    if request.method == 'POST':
+        form = CourseForm(request.POST or None, instance=request.user)
+
+        if form.is_valid():
+            course = form.save(commit=False)
+            course.full_name = form.cleaned_data['full_name']
+            course.email = form.cleaned_data['email']
+            course.ic_number = form.cleaned_data['ic_number']
+            course.save()
+
+            messages.success(request, _('Your course has been change successfully.'))
+            return HttpResponseRedirect('/')
+        else:
+            messages.warning(request, form.errors)
+
+    else:
+        form = CourseForm()
+
+    context = {
+        'title': page_title,
+        'form': form,
+        'course': course,
+        'word': word,
+    }
+
+    return render(request, 'config/country_detail.html', context)
+
+
+def language_detail(request, pk):
+    page_title = _('Change Country')
+    course = get_object_or_404(Course, id=pk)
+    word = Word.objects.all()
+
+    form = CourseForm(prefix='course')
+
+    if request.method == 'POST':
+        form = CourseForm(request.POST or None, instance=request.user)
+
+        if form.is_valid():
+            course = form.save(commit=False)
+            course.full_name = form.cleaned_data['full_name']
+            course.email = form.cleaned_data['email']
+            course.ic_number = form.cleaned_data['ic_number']
+            course.save()
+
+            messages.success(request, _('Your course has been change successfully.'))
+            return HttpResponseRedirect('/')
+        else:
+            messages.warning(request, form.errors)
+
+    else:
+        form = CourseForm()
+
+    context = {
+        'title': page_title,
+        'form': form,
+        'course': course,
+        'word': word,
+    }
+
+    return render(request, 'config/language_detail.html', context)
+
+
+def country_add(request):
+    page_title = _('Change Course')
+    # course = get_object_or_404(Course, id=pk)
+    word = Word.objects.all()
+
+    form = CourseForm(prefix='course')
+
+    if request.method == 'POST':
+        form = CourseForm(request.POST or None, instance=request.user)
+
+        if form.is_valid():
+            course = form.save(commit=False)
+            course.full_name = form.cleaned_data['full_name']
+            course.email = form.cleaned_data['email']
+            course.ic_number = form.cleaned_data['ic_number']
+            course.save()
+
+            messages.success(request, _('Your course has been change successfully.'))
+            return HttpResponseRedirect('/')
+        else:
+            messages.warning(request, form.errors)
+
+    else:
+        form = CourseForm()
+
+    context = {
+        'title': page_title,
+        'form': form,
+        'word': word,
+    }
+
+    return render(request, 'course/country_add.html', context)
+
+
+def language_add(request):
+    page_title = _('Change Course')
+    # course = get_object_or_404(Course, id=pk)
+    word = Word.objects.all()
+
+    form = CourseForm(prefix='course')
+
+    if request.method == 'POST':
+        form = CourseForm(request.POST or None, instance=request.user)
+
+        if form.is_valid():
+            course = form.save(commit=False)
+            course.full_name = form.cleaned_data['full_name']
+            course.email = form.cleaned_data['email']
+            course.ic_number = form.cleaned_data['ic_number']
+            course.save()
+
+            messages.success(request, _('Your course has been change successfully.'))
+            return HttpResponseRedirect('/')
+        else:
+            messages.warning(request, form.errors)
+
+    else:
+        form = CourseForm()
+
+    context = {
+        'title': page_title,
+        'form': form,
+        'word': word,
+    }
+
+    return render(request, 'course/language_add.html', context)
