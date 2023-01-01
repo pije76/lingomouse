d3e9b6b9ec943f023fae9f51c03d78725de406c9
Panjianom <panjul76@hotmail.com>
2023-01-01 16:59:57 +0700

    edit config

--- config/views.py
+++ config/views.py
@@ -9,6 +9,7 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import *
+from .forms import *
 
 @method_decorator(csrf_exempt, name='dispatch')
 class SetupThemeMode(View):
@@ -51,22 +52,22 @@ def country_list(request):
 
 def country_detail(request, pk):
     page_title = _('Change Country')
-    course = get_object_or_404(Course, id=pk)
+    country = get_object_or_404(Course, id=pk)
     word = Word.objects.all()
 
-    form = CourseForm(prefix='course')
+    form = CourseForm(prefix='country')
 
     if request.method == 'POST':
         form = CourseForm(request.POST or None, instance=request.user)
 
         if form.is_valid():
-            course = form.save(commit=False)
-            course.full_name = form.cleaned_data['full_name']
-            course.email = form.cleaned_data['email']
-            course.ic_number = form.cleaned_data['ic_number']
-            course.save()
+            country = form.save(commit=False)
+            country.full_name = form.cleaned_data['full_name']
+            country.email = form.cleaned_data['email']
+            country.ic_number = form.cleaned_data['ic_number']
+            country.save()
 
-            messages.success(request, _('Your course has been change successfully.'))
+            messages.success(request, _('Your country has been change successfully.'))
             return HttpResponseRedirect('/')
         else:
             messages.warning(request, form.errors)
@@ -77,7 +78,7 @@ def country_detail(request, pk):
     context = {
         'title': page_title,
         'form': form,
-        'course': course,
+        'country': country,
         'word': word,
     }
 
@@ -122,80 +123,78 @@ def country_delete(request, pk):
     country_data = get_object_or_404(Course, id=pk)
     country_data.delete()
 
-    return redirect('course:course_list')
+    return redirect('country:country_list')
 
 ################# COUNTRY VIEW #############
 def language_list(request):
     page_title = _('Languages')
-    data_language =   Language.objects.all()
+    language_list =   Language.objects.all()
 
     context = {
         'title': page_title,
-        'data_language': data_language,
+        'language_list': language_list,
     }
 
-    return render(request,'config/config_set.html', context)
+    return render(request,'config/language_list.html', context)
 
 
 def language_detail(request, pk):
     page_title = _('Change Country')
-    course = get_object_or_404(Course, id=pk)
-    word = Word.objects.all()
+    language = get_object_or_404(Language, id=pk)
 
-    form = CourseForm(prefix='course')
+    # form = LanguageForm(prefix='language')
 
     if request.method == 'POST':
-        form = CourseForm(request.POST or None, instance=request.user)
+        form = LanguageForm(request.POST or None, instance=request.user)
 
         if form.is_valid():
-            course = form.save(commit=False)
-            course.full_name = form.cleaned_data['full_name']
-            course.email = form.cleaned_data['email']
-            course.ic_number = form.cleaned_data['ic_number']
-            course.save()
+            language = form.save(commit=False)
+            language.full_name = form.cleaned_data['full_name']
+            language.email = form.cleaned_data['email']
+            language.ic_number = form.cleaned_data['ic_number']
+            language.save()
 
-            messages.success(request, _('Your course has been change successfully.'))
+            messages.success(request, _('Your language has been change successfully.'))
             return HttpResponseRedirect('/')
         else:
             messages.warning(request, form.errors)
 
     else:
-        form = CourseForm()
+        form = LanguageForm()
 
     context = {
         'title': page_title,
         'form': form,
-        'course': course,
-        'word': word,
+        'language': language,
     }
 
     return render(request, 'config/language_detail.html', context)
 
 
 def language_add(request):
-    page_title = _('Change Course')
-    # course = get_object_or_404(Course, id=pk)
+    page_title = _('Change Language')
+    # language = get_object_or_404(Language, id=pk)
     word = Word.objects.all()
 
-    form = CourseForm(prefix='course')
+    form = LanguageForm(prefix='language')
 
     if request.method == 'POST':
-        form = CourseForm(request.POST or None, instance=request.user)
+        form = LanguageForm(request.POST or None, instance=request.user)
 
         if form.is_valid():
-            course = form.save(commit=False)
-            course.full_name = form.cleaned_data['full_name']
-            course.email = form.cleaned_data['email']
-            course.ic_number = form.cleaned_data['ic_number']
-            course.save()
+            language = form.save(commit=False)
+            language.full_name = form.cleaned_data['full_name']
+            language.email = form.cleaned_data['email']
+            language.ic_number = form.cleaned_data['ic_number']
+            language.save()
 
-            messages.success(request, _('Your course has been change successfully.'))
+            messages.success(request, _('Your language has been change successfully.'))
             return HttpResponseRedirect('/')
         else:
             messages.warning(request, form.errors)
 
     else:
-        form = CourseForm()
+        form = LanguageForm()
 
     context = {
         'title': page_title,
@@ -203,4 +202,4 @@ def language_add(request):
         'word': word,
     }
 
-    return render(request, 'course/language_add.html', context)
+    return render(request, 'config/language_add.html', context)
