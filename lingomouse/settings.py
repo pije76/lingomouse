bfc007b827b369b2e36e23ffc9e4f13bd4419844
Panjianom <panjul76@hotmail.com>
2023-01-09 20:34:58 +0700

    add log & messages

--- lingomouse/settings.py
+++ lingomouse/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,7 +53,6 @@ INSTALLED_APPS = [
 	# 'whoosh_index',
     'haystack',
 
-	'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +63,6 @@ MIDDLEWARE = [
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
-	'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'lingomouse.urls'
@@ -191,3 +190,93 @@ CACHES = {
     },
 
 }
+
+
+####################################################################################################
+
+
+if DEBUG:
+    MIDDLEWARE += (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
+    INSTALLED_APPS += (
+        'debug_toolbar',
+    )
+    INTERNAL_IPS = ('127.0.0.1',)
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+    }
+
+    MESSAGE_TAGS = {
+    	messages.DEBUG: 'alert-info',
+    	messages.INFO: 'alert-info',
+    	messages.SUCCESS: 'alert-success',
+    	messages.WARNING: 'alert-warning',
+    	messages.ERROR: 'alert-danger',
+    }
+
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [os.path.join(BASE_DIR, 'templates')],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                "debug": DEBUG,
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.template.context_processors.i18n',
+                    'django.template.context_processors.media',
+                    'django.template.context_processors.static',
+                    'django.template.context_processors.tz',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+                "string_if_invalid": '<< MISSING VARIABLE "%s" >>' if DEBUG else "",
+            },
+        },
+        #    {
+        #        'BACKEND': 'accounts.pdf.PdftkEngine',
+        #        'APP_DIRS': True,
+        #    },
+    ]
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'filters': {
+            'require_debug_false': {
+                '()': 'django.utils.log.RequireDebugFalse'
+            }
+        },
+        'handlers': {
+            'mail_admins': {
+                'level': 'ERROR',
+                'filters': ['require_debug_false'],
+                'class': 'django.utils.log.AdminEmailHandler'
+            },
+            'stderr': {
+                'level': 'INFO',
+                'class': 'logging.StreamHandler',
+            },
+            'file': {
+                'level': 'DEBUG',
+                'class': 'logging.FileHandler',
+                'filename': os.path.join(BASE_DIR, "debug.log"),
+            },
+        },
+        'loggers': {
+            'django.request': {
+                'handlers': ['file'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+            'pika': {
+                'handlers': ['mail_admins'],
+                'level': 'ERROR',
+            },
+        },
+        'root': {
+            'handlers': ['stderr'],
+            'level': 'INFO',
+        },
+    }
